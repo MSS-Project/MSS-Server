1e15313c99400a3e7b7d6b2efc992abc91525b30
John Lin <linjo@us.ibm.com>
2018-11-08 03:58:17 -0500

    changing to asynchronous and variable names

--- api/controllers/cusumPlotMortalityController.js
+++ api/controllers/cusumPlotMortalityController.js
@@ -16,114 +16,109 @@ var cusumMortality = {
 
         console.log('Getting inputs...');
         //TODO: only required for unit testing
-        // req.query.startDate = "2018-01-01";
-        // req.query.endDate = "2019-01-01";
-        // req.query.formType = "E";
-        // req.query.userLevel = 2;
-        // req.query.userId = 8;
+        req.query.startDate = "2018-01-01";
+        req.query.endDate = "2019-01-01";
+        req.query.formType = "E";
+        req.query.userLevel = 2;
+        req.query.userId = 8;
 
         console.log('These are the inputs: '+req.query.startDate+', '+req.query.endDate+', '+req.query.formType+', '+req.query.userLevel+', '+req.query.userId);
 
         //TODO: Rememove above lines
         //TODO: change path to where your path is for your R-script
         var plotType = "cusumLine";
-        var fpmScatter = R(cusumMortalityPath)
+        var data = R(cusumMortalityPath)
             .data(req.query.startDate,req.query.endDate,req.query.formType,req.query.userLevel,req.query.userId,plotType)
-            .call(function(err, a) {
-                if (err) throw err;
-                res.json(a)
-            });
+            .callSync();
+                console.log(data);
+            res.json(data);
         },
 
     ucl: function (req, res) {
 
         console.log('Getting inputs...');
         //TODO: only required for unit testing
-        // req.query.startDate = "2018-01-01";
-        // req.query.endDate = "2019-01-01";
-        // req.query.formType = "E";
-        // req.query.userLevel = 2;
-        // req.query.userId = 8;
+        req.query.startDate = "2018-01-01";
+        req.query.endDate = "2019-01-01";
+        req.query.formType = "E";
+        req.query.userLevel = 2;
+        req.query.userId = 8;
 
         console.log('These are the inputs: '+req.query.startDate+', '+req.query.endDate+', '+req.query.formType+', '+req.query.userLevel+', '+req.query.userId);
 
         //TODO: Rememove above lines
         //TODO: change path to where your path is for your R-script
         var plotType = "ucl";
-        var fpmScatter = R(cusumMortalityPath)
+        var data = R(cusumMortalityPath)
             .data(req.query.startDate,req.query.endDate,req.query.formType,req.query.userLevel,req.query.userId,plotType)
-            .call(function(err, a) {
-                if (err) throw err;
-                res.json(a)
-            });
+            .callSync();
+                console.log(data);
+            res.json(data);
         },
     lcl: function (req, res) {
 
         console.log('Getting inputs...');
         //TODO: only required for unit testing
-        // req.query.startDate = "2018-01-01";
-        // req.query.endDate = "2019-01-01";
-        // req.query.formType = "E";
-        // req.query.userLevel = 2;
-        // req.query.userId = 8;
+        req.query.startDate = "2018-01-01";
+        req.query.endDate = "2019-01-01";
+        req.query.formType = "E";
+        req.query.userLevel = 2;
+        req.query.userId = 8;
 
         console.log('These are the inputs: '+req.query.startDate+', '+req.query.endDate+', '+req.query.formType+', '+req.query.userLevel+', '+req.query.userId);
 
         //TODO: Rememove above lines
         //TODO: change path to where your path is for your R-script
         var plotType = "lcl";
-        var fpmScatter = R(cusumMortalityPath)
+        var data = R(cusumMortalityPath)
             .data(req.query.startDate,req.query.endDate,req.query.formType,req.query.userLevel,req.query.userId,plotType)
-            .call(function(err, a) {
-                if (err) throw err;
-                res.json(a)
-            });
+            .callSync();
+                console.log(data);
+            res.json(data);
         },
 
     alerts: function (req, res) {
 
         console.log('Getting inputs...');
         //TODO: only required for unit testing
-        // req.query.startDate = "2018-01-01";
-        // req.query.endDate = "2019-01-01";
-        // req.query.formType = "E";
-        // req.query.userLevel = 2;
-        // req.query.userId = 8;
+        req.query.startDate = "2018-01-01";
+        req.query.endDate = "2019-01-01";
+        req.query.formType = "E";
+        req.query.userLevel = 2;
+        req.query.userId = 8;
 
         console.log('These are the inputs: '+req.query.startDate+', '+req.query.endDate+', '+req.query.formType+', '+req.query.userLevel+', '+req.query.userId);
 
         //TODO: Rememove above lines
         //TODO: change path to where your path is for your R-script
         var plotType = "alerts";
-        var fpmScatter = R(cusumMortalityPath)
+        var data = R(cusumMortalityPath)
             .data(req.query.startDate,req.query.endDate,req.query.formType,req.query.userLevel,req.query.userId,plotType)
-            .call(function(err, a) {
-                if (err) throw err;
-                res.json(a)
-            });
+            .callSync();
+                console.log(data);
+            res.json(data);
         },
 
     missing: function (req, res) {
 
         console.log('Getting inputs...');
         //TODO: only required for unit testing
-        // req.query.startDate = "2018-01-01";
-        // req.query.endDate = "2019-01-01";
-        // req.query.formType = "E";
-        // req.query.userLevel = 2;
-        // req.query.userId = 8;
+        req.query.startDate = "2018-01-01";
+        req.query.endDate = "2019-01-01";
+        req.query.formType = "E";
+        req.query.userLevel = 2;
+        req.query.userId = 8;
 
         console.log('These are the inputs: '+req.query.startDate+', '+req.query.endDate+', '+req.query.formType+', '+req.query.userLevel+', '+req.query.userId);
 
         //TODO: Rememove above lines
         //TODO: change path to where your path is for your R-script
         var plotType = "missing";
-        var fpmScatter = R(cusumMortalityPath)
+        var data = R(cusumMortalityPath)
             .data(req.query.startDate,req.query.endDate,req.query.formType,req.query.userLevel,req.query.userId,plotType)
-            .call(function(err, a) {
-                if (err) throw err;
-                res.json(a)
-            });
+            .callSync();
+                console.log(data);
+            res.json(data);
         }
 };
    
